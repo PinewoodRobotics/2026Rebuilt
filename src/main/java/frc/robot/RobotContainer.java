5b1b6223985c6f492c91b1a1525c3f4ebcf203c5
brigerodev <golden.notch.creeper@gmail.com>
2026-01-14 16:34:59 -0800

    remove useless stuff from code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,17 +1,11 @@
 package frc.robot;
 
-import java.util.function.Supplier;
-
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 import pwrup.frc.core.controller.FlightModule;
 import pwrup.frc.core.controller.FlightStick;
 import pwrup.frc.core.controller.LogitechController;
 import pwrup.frc.core.controller.OperatorPanel;
-
-import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
 
@@ -20,9 +14,8 @@ public class RobotContainer {
   final FlightStick m_leftFlightStick = new FlightStick(2);
   final FlightStick m_rightFlightStick = new FlightStick(3);
   final FlightModule m_flightModule = new FlightModule(
-    m_leftFlightStick,
-    m_rightFlightStick);
-  private Boolean isNonFieldRelative = false;
+      m_leftFlightStick,
+      m_rightFlightStick);
 
   public RobotContainer() {
     configureBindings();
