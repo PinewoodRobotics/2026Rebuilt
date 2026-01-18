feccf525294c76dac6f94ac5830674346d6e90f7
brigerodev <golden.notch.creeper@gmail.com>
2026-01-18 01:17:07 -0800

    add comments

--- src/main/java/frc/robot/constant/AutoUpdatingConstants.java
+++ src/main/java/frc/robot/constant/AutoUpdatingConstants.java
@@ -7,14 +7,17 @@ import frc.robot.Robot;
 import proto.status.Frontend.PIDFFUpdateMessage;
 
 public class AutoUpdatingConstants {
+    // These pid constants will be updated from the frontend on the topic of
+    // "PIDFFUpdate"
+    private static final String kPIDFFUpdateTopic = "PIDFFUpdate";
+
     public static double kGeneralP = 0.0;
     public static double kGeneralI = 0.0;
     public static double kGeneralD = 0.0;
     public static double kGeneralFF = 0.0;
 
     static {
-        final String kPIDFFUpdateTopic = "PIDFFUpdate";
-
+        // actually update the constants here
         Robot.getCommunicationClient().subscribe(kPIDFFUpdateTopic, NamedCallback.FromConsumer(message -> {
             PIDFFUpdateMessage update;
             try {
@@ -29,5 +32,12 @@ public class AutoUpdatingConstants {
             kGeneralD = update.getD();
             kGeneralFF = update.getFf();
         }));
+    }
+
+    private static String a; // example for another auto updating constant
+    static {
+        // do the same exact thing as above but for the other auto updating constant
+        // make sure to never do this to just one thing and bundle multiple constants
+        // into one proto message instead.
     }
 }
