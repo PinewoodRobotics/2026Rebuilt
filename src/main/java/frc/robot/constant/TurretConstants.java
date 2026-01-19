39db5136ecef64005df15c4ae444e3b64258f3fa
brigerodev <golden.notch.creeper@gmail.com>
2026-01-19 15:28:55 -0800

    add tuning

--- src/main/java/frc/robot/constant/TurretConstants.java
+++ src/main/java/frc/robot/constant/TurretConstants.java
@@ -16,8 +16,8 @@ public class TurretConstants {
 	public static final double kTurretMotorRotationsPerRotation = 16.0;
 	public static final MotorType kTurretMotorType = MotorType.kBrushless;
 
-	public static final double kTurretP = 0.2;
-	public static final double kTurretI = 0.0;
+	public static final double kTurretP = 1;
+	public static final double kTurretI = 0.01;
 	public static final double kTurretD = 0.0;
 	public static final double kTurretIZ = 0.0;
 
