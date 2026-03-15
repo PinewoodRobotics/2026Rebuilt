4b99f5c032c2eac10ad0b6516bca049ce1095e9f
brigerodev <golden.notch.creeper@gmail.com>
2026-03-14 19:20:37 -0700

    update abs encoder start pos

--- src/main/java/frc/robot/constant/IntakeConstants.java
+++ src/main/java/frc/robot/constant/IntakeConstants.java
@@ -20,7 +20,7 @@ public class IntakeConstants {
 
   public final static int intakeWristCurrentLimit = 20;
   public final static double intakeWristGearingRatio = 1.0 / 80.0;
-  public final static Rotation2d intakeWristOffset = Rotation2d.fromRotations(0.015); // when the wrist is fully down
+  public final static Rotation2d intakeWristOffset = Rotation2d.fromRotations(0.0175); // when the wrist is fully down
 
   public static enum WristRaiseLocation {
     BOTTOM(Rotation2d.fromRotations(0)),
